639179d41369e0b0690e268b942644d12aadd049
Ashish Pansheriya <www.ashish.exe@gmail.com>
2016-07-08 17:50:18 +0530

    Delete tabs.js

--- www/js/controllers/tabs.js
+++
@@ -1,7 +0,0 @@
-angular.module('cosmic.controllers').controller('TabsCtrl', function($scope, $ionicSideMenuDelegate) {
-   
-  $scope.openMenu = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  }
-   
-});
