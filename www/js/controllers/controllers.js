b540a630fa7fda8de81f4c3316fd5c0962d5cf06
Ashish Pansheriya <www.ashish.exe@gmail.com>
2016-07-08 22:02:09 +0530

    created 3 controllers

---
+++ www/js/controllers/controllers.js
@@ -0,0 +1,15 @@
+
+ angular.module('cosmic.controllers', [])
+ 
+.controller('browseCtrl', function($scope) {
+
+})
+   
+.controller('searchCloudCtrl', function($scope) {
+
+})
+   
+.controller('shareCtrl', function($scope) {
+
+});
+ 
